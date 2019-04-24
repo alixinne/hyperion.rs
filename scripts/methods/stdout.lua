b2edadfcb6726d2ec3b169e942ac408b21d14b9a
Vincent Tavernier <vince.tavernier@gmail.com>
2019-04-24 19:55:36 +0200

    Arbitrary bit depth for stdout method

--- scripts/methods/stdout.lua
+++ scripts/methods/stdout.lua
@@ -1,11 +1,18 @@
-function to10bit(x)
-	return math.floor(4095 * x)
+pdebug("host version: " .. hyperion_params.host.version)
+pdebug("lua stdout method initializing")
+
+if hyperion_params["bits"] ~= nil then
+	bits = hyperion_params.bits
+else
+	bits = 8
+end
+
+function tobits(x)
+	return math.floor(((1 << bits) - 1) * x)
 end
 
 function write(leds)
 	for k,v in pairs(leds) do
-		pinfo("LED" .. tostring(k - 1) .. " r=" .. tostring(to10bit(v.r)) .. " g=" .. tostring(to10bit(v.g)) .. " b=" .. tostring(to10bit(v.b)))
+		pinfo("LED" .. tostring(k - 1) .. " r=" .. tostring(tobits(v.r)) .. " g=" .. tostring(tobits(v.g)) .. " b=" .. tostring(tobits(v.b)))
 	end
 end
-
-pdebug("host version: " .. hyperion_params.host.version)

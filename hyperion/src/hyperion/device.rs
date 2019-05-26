9193d9ef72250f335405c77a24fa13ddbed5cf3a
Vincent Tavernier <vince.tavernier@gmail.com>
2019-05-26 18:46:00 +0200

    Add device sanitize tests

--- hyperion/src/hyperion/device.rs
+++ hyperion/src/hyperion/device.rs
@@ -232,6 +232,37 @@ mod tests {
     use super::*;
 
     #[test]
+    fn sanitize_frequency() {
+        let mut device = Device {
+            name: "test".into(),
+            endpoint: Endpoint::Stdout { bits: 8 },
+            leds: Vec::new(),
+            frequency: -2.0,
+            idle: IdleSettings::default(),
+        };
+
+        device.sanitize();
+        assert!(device.frequency >= 1.0f64 / 3600f64);
+    }
+
+    #[test]
+    fn sanitize_idle() {
+        let mut device = Device {
+            name: "test".into(),
+            endpoint: Endpoint::Stdout { bits: 8 },
+            leds: Vec::new(),
+            frequency: 1.0,
+            idle: IdleSettings {
+                delay: Duration::from_millis(5),
+                .. Default::default()
+            },
+        };
+
+        device.sanitize();
+        assert!(device.idle.delay > Duration::from_millis((1_000f64 / device.frequency) as u64));
+    }
+
+    #[test]
     fn serialize_udp_endpoint() {
         let endpoint = Endpoint::Udp {
             address: "127.0.0.1:19446".into(),

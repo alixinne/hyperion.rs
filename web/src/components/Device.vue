9678fdb327a98e855d342710515dc6c869ab3f8c
Vincent Tavernier <vince.tavernier@gmail.com>
2019-09-16 18:12:06 +0200

    web: progress on device UI

--- web/src/components/Device.vue
+++ web/src/components/Device.vue
@@ -6,7 +6,8 @@
           <div class="level-item">
             <b-field v-bind:type="$v.deviceName.$error ? 'is-danger' : ''">
               <b-input placeholder="Device name"
-                       v-model="$v.deviceName.$model">
+                       v-model="$v.deviceName.$model"
+                       v-on:input="updateDeviceName">
               </b-input>
             </b-field>
           </div>
@@ -19,7 +20,38 @@
       </div>
     </div>
     <div class="panel-block">
-      <p>{{ device.endpoint }}</p>
+      <div class="columns device-field-columns is-vcentered">
+        <div class="column device-field-label has-text-right-tablet is-one-fifth-tablet"><strong>Endpoint</strong></div>
+        <div class="column">{{ device.endpoint }}</div>
+      </div>
+    </div>
+    <div class="panel-block">
+      <div class="columns device-field-columns is-vcentered">
+        <div class="column device-field-label has-text-right-tablet is-one-fifth-tablet"><strong>Frequency</strong></div>
+        <div class="column">
+          <b-field>
+            <b-slider :min="1" :max="120" ticks v-model="device.frequency"></b-slider>
+          </b-field>
+        </div>
+      </div>
+    </div>
+    <div class="panel-block">
+      <div class="columns device-field-columns is-vcentered">
+        <div class="column device-field-label has-text-right-tablet is-one-fifth-tablet"><strong>Filter</strong></div>
+        <div class="column">{{ device.filter }}</div>
+      </div>
+    </div>
+    <div class="panel-block">
+      <div class="columns device-field-columns is-vcentered">
+        <div class="column device-field-label has-text-right-tablet is-one-fifth-tablet"><strong>Idle settings</strong></div>
+        <div class="column">{{ device.idle }}</div>
+      </div>
+    </div>
+    <div class="panel-block">
+      <div class="columns device-field-columns is-vcentered">
+        <div class="column device-field-label has-text-right-tablet is-one-fifth-tablet"><strong>Color format</strong></div>
+        <div class="column">{{ device.format }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -47,15 +79,28 @@ export default {
   mounted () {
     this.deviceName = this.device.name
   },
-  watch: {
-    deviceName: _.debounce(function (name) {
-      if (this.$v.$anyError) {
+  methods: {
+    startBusy () {
+      this.busy = true
+    },
+    endBusy (message, isError) {
+      this.busy = false
+      if (message) {
+        this.$buefy.toast.open({
+          message,
+          type: isError ? 'is-danger' : 'is-success'
+        })
+      }
+    },
+    updateDeviceName: _.debounce(function (name) {
+      this.$v.$touch()
+      if (this.$v.deviceName.$anyError) {
         return
       }
 
-      this.busy = true
+      this.startBusy()
       this.$store.dispatch('devices/patch', { id: this.id, patch: { name } }).finally(() => {
-        this.busy = false
+        this.endBusy(this.device.name + ' renamed to ' + name + ' successfully!', false)
       })
     }, 500)
   },
@@ -75,5 +120,18 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.device-field-columns {
+  padding-top: 0;
+  padding-bottom: 0;
+  width: 100%;
+}
+
+.device-field-columns > .device-field-label {
+}
+
+.device-field-columns > .column {
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+}
 </style>

7bf1f0bf94e960f6938ecc6a6b955e75a9445f4d
Vincent Tavernier <vince.tavernier@gmail.com>
2021-02-05 15:30:08 +0100

    Skip rustfmt for FlatBuffers generated code

--- src/servers/flat/message.rs
+++ src/servers/flat/message.rs
@@ -1,6 +1,8 @@
 #[allow(unused_imports)]
+#[rustfmt::skip]
 mod hyperion_reply_generated;
 #[allow(unused_imports)]
+#[rustfmt::skip]
 mod hyperion_request_generated;
 
 pub use hyperion_reply_generated::hyperionnet::*;

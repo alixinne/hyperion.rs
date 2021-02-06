e01dd3a118bd5ec81226f328b25b953cf8081429
Vincent Tavernier <vince.tavernier@gmail.com>
2021-02-06 22:07:43 +0100

    Limit the number of runtime threads

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate log;
 
 use structopt::StructOpt;
-use tokio::runtime::Runtime;
+use tokio::runtime::Builder;
 use tokio::signal;
 
 #[derive(Debug, StructOpt)]
@@ -11,6 +11,131 @@ struct Opts {
     verbose: u32,
     #[structopt(long)]
     dump_config: bool,
+}
+
+async fn run(config: hyperion::models::Config) -> color_eyre::eyre::Result<()> {
+    // Create the global state object
+    let global = hyperion::global::GlobalData::new().wrap();
+
+    // Initialize and spawn the devices
+    let mut initialized_devices = 0;
+    let mut max_id = 0;
+
+    for (id, inst) in &config.instances {
+        max_id = max_id.max(*id);
+
+        match hyperion::instance::Instance::new(global.clone(), inst.clone()).await {
+            Ok(inst) => {
+                initialized_devices += 1;
+
+                // TODO: Move this code to a function
+                tokio::spawn(async move {
+                    let result = inst.run().await;
+
+                    if let Err(error) = result {
+                        error!("Instance error: {:?}", error);
+                    }
+                });
+            }
+            Err(error) => {
+                error!(
+                    "Initializing instance {} `{}` failed: {}",
+                    id, inst.instance.friendly_name, error
+                );
+            }
+        }
+    }
+
+    // Add a dummy device for debugging if needed
+    if initialized_devices == 0 {
+        warn!("no devices were initialized, adding a dummy device");
+        let inst = hyperion::instance::Instance::new(
+            global.clone(),
+            hyperion::models::InstanceConfig::new_dummy(max_id + 1),
+        )
+        .await?;
+
+        // TODO: Move this code to a function
+        tokio::spawn(async move {
+            let result = inst.run().await;
+
+            if let Err(error) = result {
+                error!("Instance error: {:?}", error);
+            }
+        });
+    }
+
+    // Start the flatbuffers servers
+    if config.global.flatbuffers_server.enable {
+        tokio::spawn({
+            let global = global.clone();
+            let config = config.global.flatbuffers_server.clone();
+
+            async move {
+                let result =
+                    hyperion::servers::bind(config, global, hyperion::servers::flat::handle_client)
+                        .await;
+
+                if let Err(error) = result {
+                    error!("Flatbuffers server terminated: {:?}", error);
+                }
+            }
+        });
+    }
+
+    // Start the JSON server
+    tokio::spawn({
+        let global = global.clone();
+        let config = config.global.json_server.clone();
+
+        async move {
+            let result =
+                hyperion::servers::bind(config, global, hyperion::servers::json::handle_client)
+                    .await;
+
+            if let Err(error) = result {
+                error!("JSON server terminated: {:?}", error);
+            }
+        }
+    });
+
+    // Start the Protobuf server
+    if config.global.proto_server.enable {
+        tokio::spawn({
+            let global = global.clone();
+            let config = config.global.proto_server.clone();
+
+            async move {
+                let result = hyperion::servers::bind(
+                    config,
+                    global,
+                    hyperion::servers::proto::handle_client,
+                )
+                .await;
+
+                if let Err(error) = result {
+                    error!("Protobuf server terminated: {:?}", error);
+                }
+            }
+        });
+    }
+
+    // Spawn priority muxer
+    let muxer = hyperion::muxer::PriorityMuxer::new(global.clone()).await;
+    tokio::spawn(muxer.run());
+
+    // Should we continue running?
+    let mut abort = false;
+
+    while !abort {
+        tokio::select! {
+            _ = signal::ctrl_c() => {
+                abort = true;
+            }
+        }
+    }
+
+    Ok(())
 }
 
 #[paw::main]
@@ -46,133 +171,15 @@ fn main(opts: Opts) -> color_eyre::eyre::Result<()> {
     }
 
     // Create tokio runtime
-    let rt = Runtime::new()?;
-
-    rt.block_on(async move {
-        // Create the global state object
-        let global = hyperion::global::GlobalData::new().wrap();
-
-        // Initialize and spawn the devices
-        let mut initialized_devices = 0;
-        let mut max_id = 0;
-
-        for (id, inst) in &config.instances {
-            max_id = max_id.max(*id);
-
-            match hyperion::instance::Instance::new(global.clone(), inst.clone()).await {
-                Ok(inst) => {
-                    initialized_devices += 1;
-
-                    // TODO: Move this code to a function
-                    tokio::spawn(async move {
-                        let result = inst.run().await;
-
-                        if let Err(error) = result {
-                            error!("Instance error: {:?}", error);
-                        }
-                    });
-                }
-                Err(error) => {
-                    error!(
-                        "Initializing instance {} `{}` failed: {}",
-                        id, inst.instance.friendly_name, error
-                    );
-                }
-            }
-        }
-
-        // Add a dummy device for debugging if needed
-        if initialized_devices == 0 {
-            warn!("no devices were initialized, adding a dummy device");
-            let inst = hyperion::instance::Instance::new(
-                global.clone(),
-                hyperion::models::InstanceConfig::new_dummy(max_id + 1),
-            )
-            .await?;
-
-            // TODO: Move this code to a function
-            tokio::spawn(async move {
-                let result = inst.run().await;
-
-                if let Err(error) = result {
-                    error!("Instance error: {:?}", error);
-                }
-            });
-        }
-
-        // Start the flatbuffers servers
-        if config.global.flatbuffers_server.enable {
-            tokio::spawn({
-                let global = global.clone();
-                let config = config.global.flatbuffers_server.clone();
-
-                async move {
-                    let result = hyperion::servers::bind(
-                        config,
-                        global,
-                        hyperion::servers::flat::handle_client,
-                    )
-                    .await;
-
-                    if let Err(error) = result {
-                        error!("Flatbuffers server terminated: {:?}", error);
-                    }
-                }
-            });
-        }
-
-        // Start the JSON server
-        tokio::spawn({
-            let global = global.clone();
-            let config = config.global.json_server.clone();
-
-            async move {
-                let result =
-                    hyperion::servers::bind(config, global, hyperion::servers::json::handle_client)
-                        .await;
-
-                if let Err(error) = result {
-                    error!("JSON server terminated: {:?}", error);
-                }
-            }
-        });
-
-        // Start the Protobuf server
-        if config.global.proto_server.enable {
-            tokio::spawn({
-                let global = global.clone();
-                let config = config.global.proto_server.clone();
-
-                async move {
-                    let result = hyperion::servers::bind(
-                        config,
-                        global,
-                        hyperion::servers::proto::handle_client,
-                    )
-                    .await;
-
-                    if let Err(error) = result {
-                        error!("Protobuf server terminated: {:?}", error);
-                    }
-                }
-            });
-        }
-
-        // Spawn priority muxer
-        let muxer = hyperion::muxer::PriorityMuxer::new(global.clone()).await;
-        tokio::spawn(muxer.run());
-
-        // Should we continue running?
-        let mut abort = false;
-
-        while !abort {
-            tokio::select! {
-                _ = signal::ctrl_c() => {
-                    abort = true;
-                }
-            }
-        }
-
-        Ok(())
-    })
+    let thd_count = num_cpus::get().min(4);
+    if thd_count > 1 {
+        let rt = Builder::new_multi_thread()
+            .worker_threads(thd_count)
+            .enable_all()
+            .build()?;
+        rt.block_on(run(config))
+    } else {
+        let rt = Builder::new_current_thread().enable_all().build()?;
+        rt.block_on(run(config))
+    }
 }

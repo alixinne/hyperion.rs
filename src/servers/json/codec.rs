e91cbbcfb5fe2aa01c693438f8fe3dbdb9a00326
Vincent Tavernier <vince.tavernier@gmail.com>
2021-07-05 17:46:48 +0200

    refactor(api): Simplify JSON codec

--- src/servers/json/codec.rs
+++ src/servers/json/codec.rs
@@ -4,32 +4,6 @@ use tokio_util::codec::{Decoder, Encoder, LinesCodec};
 
 use crate::api::json::message;
 
-/// Parse an incoming request as JSON into the corresponding message type
-///
-/// # Parameters
-///
-/// * `line`: input request line to parse as a message
-///
-/// # Errors
-///
-/// When the line cannot be parsed as JSON, the underlying error is returned from serde_json.
-fn parse_request(line: &str) -> serde_json::Result<message::HyperionMessage> {
-    serde_json::from_str(line)
-}
-
-/// Encode an outgoing reply as JSON
-///
-/// # Parameters
-///
-/// * `reply`: reply to encode as JSON
-///
-/// # Errors
-///
-/// When the reply cannot be encoded as JSON, the underlying error is returned from serde_json.
-fn encode_reply(reply: &message::HyperionResponse) -> serde_json::Result<String> {
-    serde_json::to_string(reply)
-}
-
 #[derive(Debug, Error)]
 pub enum JsonCodecError {
     #[error("i/o error: {0}")]
@@ -62,7 +36,7 @@ impl Decoder for JsonCodec {
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         match self.lines.decode(src) {
             Ok(lines_result) => Ok(match lines_result {
-                Some(ref line) => Some(parse_request(line)?),
+                Some(ref line) => Some(serde_json::from_str(line)?),
                 None => None,
             }),
             Err(error) => Err(error.into()),
@@ -78,7 +52,7 @@ impl Encoder<message::HyperionResponse> for JsonCodec {
         item: message::HyperionResponse,
         dst: &mut BytesMut,
     ) -> Result<(), Self::Error> {
-        match encode_reply(&item) {
+        match serde_json::to_string(&item) {
             Ok(encoded) => Ok(self.lines.encode(encoded, dst)?),
             Err(encode_error) => Err(encode_error.into()),
         }

028c2cdd1b48ed6ac3da836d38370c66d7421b75
vtavernier <vtavernier@users.noreply.github.com>
2021-10-25 19:59:16 +0000

    deploy: d44dc8adce5df39542eb5445581ebf3955ea12ee

--- hyperion/instance/device/ws2812spi/sidebar-items.js
+++ hyperion/instance/device/ws2812spi/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["BITPAIR_TO_BYTE",""],["SPI_BYTES_PER_COLOUR",""],["SPI_BYTES_PER_LED",""],["SPI_FRAME_END_LATCH_BYTES",""]],"struct":[["Ws2812SpiImpl",""]],"type":[["Ws2812SpiDevice",""]]});
+initSidebarItems({"constant":[["BITPAIR_TO_BYTE",""],["SPI_BYTES_PER_COLOUR",""],["SPI_BYTES_PER_LED",""],["SPI_FRAME_END_LATCH_BYTES",""]],"enum":[["ImplState",""]],"struct":[["Ws2812SpiImpl",""]],"type":[["Ws2812SpiDevice",""]]});

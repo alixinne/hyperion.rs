03b8ff3a0aebe53ffcb5d4d073174df1c5b40104
Vincent Tavernier <v.tavernier@pm.me>
2023-12-10 00:14:17 +0100

    chore: fix update issues

--- src/api/flat/message.rs
+++ src/api/flat/message.rs
@@ -1,9 +1,7 @@
-#[allow(unused_imports)]
-#[allow(clippy::extra_unused_lifetimes)]
+#[allow(unused_imports, clippy::extra_unused_lifetimes, clippy::missing_safety_doc, clippy::needless_lifetimes)]
 #[rustfmt::skip]
 mod hyperion_reply_generated;
-#[allow(unused_imports)]
-#[allow(clippy::extra_unused_lifetimes)]
+#[allow(unused_imports, clippy::extra_unused_lifetimes, clippy::missing_safety_doc, clippy::needless_lifetimes)]
 #[rustfmt::skip]
 mod hyperion_request_generated;
 

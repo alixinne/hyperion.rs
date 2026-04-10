0534a254db738688759abd9b291dc115bd098fc1
Alixinne <alixinne@pm.me>
2026-04-11 01:44:40 +0200

    chore: bump version to 0.3.2

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       system:
       let
         # TODO: Update this on new releases
-        version = "0.3.1";
+        version = "0.3.2";
 
         pkgs = nixpkgs.legacyPackages.${system};
         raspberryPiZeroTarget = "arm-unknown-linux-gnueabihf";
